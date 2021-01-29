3c42edc58eba70ad16f9ef59b2abba1846be1a8e
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-01-29 11:57:26 +0300

    Initialize hilt in main application component

---
+++ app/src/main/java/com/ricknmorty/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.ricknmorty
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Ibrah on 1/29/21.
+ */
+@HiltAndroidApp
+class MainApplication :Application()
