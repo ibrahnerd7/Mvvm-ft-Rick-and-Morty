7dee0fdc9e62d86786c98a43a3ca6dc18c2b30a1
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-01-29 15:59:29 +0300

    Create character DAO

---
+++ app/src/main/java/com/ricknmorty/data/local/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.ricknmorty.data.local
+
+import androidx.room.RoomDatabase
+
+/**
+ * Created by Ibrah on 1/29/21.
+ */
+abstract class AppDatabase :RoomDatabase(){
+}
---
+++ app/src/main/java/com/ricknmorty/data/local/CharacterDao.kt
@@ -0,0 +1,27 @@
+package com.ricknmorty.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ricknmorty.data.entities.Character
+
+/**
+ * Created by Ibrah on 1/29/21.
+ */
+@Dao
+interface CharacterDao {
+    @Query("SELECT * FROM characters")
+    fun getAllCharacters(): LiveData<List<Character>>
+
+    @Query("SELECT * FROM characters WHERE id = :id")
+    fun getCharacter(id: Int): LiveData<Character>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(character: List<Character>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(character: Character)
+
+}
