44f6c0c262bae07e75732e90cc07c60a99bdcde3
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-01-29 16:34:57 +0300

    Provide database ,dao and databasemodules

--- app/src/main/java/com/ricknmorty/data/entities/di/AppModule.kt
+++ app/src/main/java/com/ricknmorty/data/entities/di/AppModule.kt
@@ -3,8 +3,11 @@ package com.ricknmorty.data.entities.di
 import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.ricknmorty.data.local.AppDatabase
+import com.ricknmorty.data.local.CharacterDao
 import com.ricknmorty.data.remote.CharacterRemoteDataSource
 import com.ricknmorty.data.remote.CharacterService
+import com.ricknmorty.data.repository.CharacterRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,10 +36,28 @@ object AppModule {
     fun provideGson(): Gson = GsonBuilder().create()
 
     @Provides
-    fun provideCharacterService(retrofit: Retrofit):CharacterService=retrofit.create(CharacterService::class.java)
+    fun provideCharacterService(retrofit: Retrofit): CharacterService =
+        retrofit.create(CharacterService::class.java)
 
     @Singleton
     @Provides
-    fun provideCharacterRemoteDataSource(characterService: CharacterService)=CharacterRemoteDataSource(characterService)
+    fun provideCharacterRemoteDataSource(characterService: CharacterService) =
+        CharacterRemoteDataSource(characterService)
 
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext appContext: Context) =
+        AppDatabase.getDatabase(appContext)
+
+    @Singleton
+    @Provides
+    fun provideCharacterDao(db: AppDatabase) = db.characterDao()
+
+    @Singleton
+    @Provides
+    fun provideRepository(
+        remoteDataSource: CharacterRemoteDataSource,
+        localDataSource: CharacterDao
+    ) =
+        CharacterRepository(remoteDataSource, localDataSource)
 }
