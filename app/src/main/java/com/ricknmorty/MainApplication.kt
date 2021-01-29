42c600dc38202f7aad9f6b5592fc40d267c25538
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-01-29 11:58:52 +0300

    Activate logs by timber in debug mode

--- app/src/main/java/com/ricknmorty/MainApplication.kt
+++ app/src/main/java/com/ricknmorty/MainApplication.kt
@@ -1,10 +1,19 @@
 package com.ricknmorty
 
 import android.app.Application
+import androidx.viewbinding.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 /**
  * Created by Ibrah on 1/29/21.
  */
 @HiltAndroidApp
-class MainApplication :Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
